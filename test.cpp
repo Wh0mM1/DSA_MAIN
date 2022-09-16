960350013507e3cae517c39eef5101dcbd10f30f
Nihal <shaikhnihal92@gmail.com>
2022-09-16 10:51:45 +0530

    change

--- test.cpp
+++ test.cpp
@@ -1,53 +1,19 @@
 #include <iostream>
 using namespace std;
 
+int gcd(int a,int b)
+{
+    if(a==0 || b==0)
+    {
+        return max(a,b);
+    }
+    int maxAB=max(a,b),minAB=min(a,b);
+    return gcd(maxAB-minAB,minAB);
+}
+
 int main()
 {
-    int n1,n2,n3;
-    cin>>n1>>n2>>n3;
-    int m1[n1][n2],m2[n2][n3];
-    for(int i=0;i<n1;i++)
-    {
-        for(int j=0;j<n2;j++)
-        {
-            cin>>m1[i][j];
-        }
-    }
-    for(int i=0;i<n2;i++)
-    {
-        for(int j=0;j<n3;j++)
-        {
-            cin>>m2[i][j];
-        }
-    }
-
-    int ans[n1][n3];
-    for(int i=0;i<n1;i++)
-    {
-        for(int j=0;j<n3;j++)
-        {
-            ans[i][j]=0;
-        }
-    }
-
-    for(int i=0;i<n1;i++)
-    {
-        for(int j=0;j<n3;j++)
-        {
-            for(int k=0;k<n2;k++)
-            {
-                ans[i][j]+=m1[i][k]*m2[k][j];
-            }
-        }
-    }
-
-    for(int i=0;i<n1;i++)
-    {
-        for(int j=0;j<n3;j++)
-        {
-            cout<<ans[i][j]<<" ";
-        }
-        cout<<endl;
-    }
+    int a=4,b=10;
+    cout<<gcd(a,b)<<endl;
     return 0;
 }
